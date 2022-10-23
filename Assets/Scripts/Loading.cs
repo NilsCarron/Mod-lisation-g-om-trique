7a1bf65f32fdcb9d664aebb8cfe7ee60d56e9303
NilsCarron <nilscarron@yahoo.fr>
2022-10-23 19:15:42 +0200

    Normals and Saving Works

--- Assets/Scripts/Loading.cs
+++ Assets/Scripts/Loading.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using static System.Math;
 
@@ -14,13 +15,14 @@ public class Loading : MonoBehaviour
     public int nbCubes;
     
     
-    
+    private List<Vector3> normales = new List<Vector3>();
+
     
     
     
     private void Start()
     {
-
+        
         string content = file.text;
         string[] splitContent = content.Split("\n");
 
@@ -96,14 +98,12 @@ public class Loading : MonoBehaviour
             pointPositif.z = pointNegatif.z + (sizeArete );
 
         }
-        
+       
         
         Vector3 pointPositifCube = Vector3.zero;
         Vector3 pointNegatifCube = Vector3.zero;
 
 
-        GenerateCube(pointNegatif, 0.01f);
-        GenerateCube(pointPositif, 0.01f);
         
         for (int indexZ = 0; indexZ <= nbCubes; indexZ++)
         {
@@ -151,6 +151,27 @@ public class Loading : MonoBehaviour
             triangles[compteur] = int.Parse(coords[1]);
             triangles[compteur + 1] = int.Parse(coords[2]);
             triangles[compteur + 2] = int.Parse(coords[3]);
+            
+            //Calcul de la normale
+            Vector3 centre = Vector3.zero;
+                
+            centre.x = (vertices[int.Parse(coords[1])].x + vertices[int.Parse(coords[2])].x + vertices[int.Parse(coords[3])].x) / 3;
+            centre.y = (vertices[int.Parse(coords[1])].y + vertices[int.Parse(coords[2])].y + vertices[int.Parse(coords[3])].y) / 3;
+            centre.z = (vertices[int.Parse(coords[1])].z + vertices[int.Parse(coords[2])].z + vertices[int.Parse(coords[3])].z) / 3;
+
+            Vector3 normale = Vector3.zero;
+
+            Vector3 S1S2 = vertices[int.Parse(coords[2])] - vertices[int.Parse(coords[1])];
+            Vector3 S1S3 = vertices[int.Parse(coords[3])] - vertices[int.Parse(coords[1])];
+
+            normale = Vector3.Cross(S1S2, S1S3);
+
+            normale = centre + normale;
+
+            normales.Add(centre);
+            normales.Add(normale);
+            
+            
             compteur += 3;
         }
 
@@ -160,10 +181,55 @@ public class Loading : MonoBehaviour
 
         msh.vertices = vertices;
         msh.triangles = triangles;
-
         gameObject.GetComponent<MeshFilter>().mesh = msh;
         gameObject.GetComponent<MeshRenderer>().material = mat;
+        
+        Save();
 
+        
+    }
+    public void Save()
+    {       
+
+        string path = Application.persistentDataPath + "/objectBroken.txt";
+        Debug.Log(path);
+
+        if (File.Exists(path))
+        {
+            File.WriteAllText(path, "");
+        }
+
+        Vector3[] vertices = gameObject.GetComponent<MeshFilter>().mesh.vertices;
+        int[] triangles = gameObject.GetComponent<MeshFilter>().mesh.triangles;
+
+        StreamWriter writer = new StreamWriter(path, true);
+
+        writer.WriteLine("OFF");
+
+        string infos = vertices.Length + " " + triangles.Length / 3 + " " + 0;
+        writer.WriteLine(infos);
+        
+        for(int i = 0; i < vertices.Length; i++)
+        {
+            string coords = "";
+            coords += vertices[i].x + " ";
+            coords += vertices[i].y + " ";
+            coords += vertices[i].z;
+            writer.WriteLine(coords);
+        }
+
+        for(int i = 0; i < triangles.Length; i+=3)
+        {
+            string coords = "";
+            coords += "3 ";
+            coords += triangles[i] + " ";
+            coords += triangles[i+1] + " ";
+            coords += triangles[i+2];
+            writer.WriteLine(coords);
+        }
+        Debug.Log("fdsfdsf");
+
+        writer.Close();
     }
 
  public void GenerateCube(Vector3 coordonneesBox, float tailleCube)
@@ -253,5 +319,19 @@ public class Loading : MonoBehaviour
 
 
     }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        if (gameObject.GetComponent<MeshFilter>() != null)
+        {
+            if (normales != null)
+            {
+                for (int i = 0; i < normales.Count; i+=2)
+                {
+                    Gizmos.DrawLine(normales[i], normales[i + 1]);
+                }
+            }
+        }
 
+    }
 }
